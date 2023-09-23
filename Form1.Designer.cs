47439dd5426f5b7c43586f450c6025781ed149fb
Rayan Perera <rayanp2004@gmail.com>
2023-09-23 22:28:43 +0530

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace Quiz_Game_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            btn1 = new Button();
+            btn2 = new Button();
+            btn3 = new Button();
+            btn4 = new Button();
+            label3 = new Label();
+            btn5 = new Button();
+            btn6 = new Button();
+            btn7 = new Button();
+            btn8 = new Button();
+            label4 = new Label();
+            btn9 = new Button();
+            btn10 = new Button();
+            btn11 = new Button();
+            btn12 = new Button();
+            label5 = new Label();
+            btn13 = new Button();
+            btn14 = new Button();
+            btn15 = new Button();
+            btn16 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Black", 18F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label1.Location = new Point(322, 50);
+            label1.Name = "label1";
+            label1.Size = new Size(130, 32);
+            label1.TabIndex = 0;
+            label1.Text = "POP QUIZ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(78, 130);
+            label2.Name = "label2";
+            label2.Size = new Size(212, 15);
+            label2.TabIndex = 1;
+            label2.Text = "How many seconds are there in a year?";
+            // 
+            // btn1
+            // 
+            btn1.Location = new Point(78, 161);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(230, 34);
+            btn1.TabIndex = 7;
+            btn1.Text = "12";
+            btn1.UseVisualStyleBackColor = true;
+            btn1.Click += btn1_Click;
+            // 
+            // btn2
+            // 
+            btn2.Location = new Point(314, 161);
+            btn2.Name = "btn2";
+            btn2.Size = new Size(230, 34);
+            btn2.TabIndex = 8;
+            btn2.Text = "360";
+            btn2.UseVisualStyleBackColor = true;
+            btn2.Click += btn2_Click;
+            // 
+            // btn3
+            // 
+            btn3.Location = new Point(78, 201);
+            btn3.Name = "btn3";
+            btn3.Size = new Size(230, 34);
+            btn3.TabIndex = 9;
+            btn3.Text = "6";
+            btn3.UseVisualStyleBackColor = true;
+            btn3.Click += btn3_Click;
+            // 
+            // btn4
+            // 
+            btn4.Location = new Point(314, 201);
+            btn4.Name = "btn4";
+            btn4.Size = new Size(230, 34);
+            btn4.TabIndex = 10;
+            btn4.Text = "25";
+            btn4.UseVisualStyleBackColor = true;
+            btn4.Click += btn4_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(79, 261);
+            label3.Name = "label3";
+            label3.Size = new Size(200, 15);
+            label3.TabIndex = 11;
+            label3.Text = "What has keys but can't open locks ?";
+            label3.Click += label3_Click;
+            // 
+            // btn5
+            // 
+            btn5.Location = new Point(78, 307);
+            btn5.Name = "btn5";
+            btn5.Size = new Size(230, 34);
+            btn5.TabIndex = 12;
+            btn5.Text = "Alan Keys";
+            btn5.UseVisualStyleBackColor = true;
+            btn5.Click += btn5_Click;
+            // 
+            // btn6
+            // 
+            btn6.Location = new Point(314, 307);
+            btn6.Name = "btn6";
+            btn6.Size = new Size(230, 34);
+            btn6.TabIndex = 13;
+            btn6.Text = "None";
+            btn6.UseVisualStyleBackColor = true;
+            btn6.Click += btn6_Click;
+            // 
+            // btn7
+            // 
+            btn7.Location = new Point(78, 347);
+            btn7.Name = "btn7";
+            btn7.Size = new Size(230, 34);
+            btn7.TabIndex = 14;
+            btn7.Text = "Car Keys";
+            btn7.UseVisualStyleBackColor = true;
+            btn7.Click += btn7_Click;
+            // 
+            // btn8
+            // 
+            btn8.Location = new Point(314, 347);
+            btn8.Name = "btn8";
+            btn8.Size = new Size(230, 34);
+            btn8.TabIndex = 15;
+            btn8.Text = "A Piano";
+            btn8.UseVisualStyleBackColor = true;
+            btn8.Click += btn8_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(83, 421);
+            label4.Name = "label4";
+            label4.Size = new Size(161, 15);
+            label4.TabIndex = 16;
+            label4.Text = "What is the capital of France?";
+            // 
+            // btn9
+            // 
+            btn9.Location = new Point(78, 457);
+            btn9.Name = "btn9";
+            btn9.Size = new Size(230, 34);
+            btn9.TabIndex = 17;
+            btn9.Text = " Madrid";
+            btn9.UseVisualStyleBackColor = true;
+            btn9.Click += btn9_Click;
+            // 
+            // btn10
+            // 
+            btn10.Location = new Point(314, 457);
+            btn10.Name = "btn10";
+            btn10.Size = new Size(230, 34);
+            btn10.TabIndex = 18;
+            btn10.Text = "Paris";
+            btn10.UseVisualStyleBackColor = true;
+            btn10.Click += btn10_Click;
+            // 
+            // btn11
+            // 
+            btn11.Location = new Point(78, 497);
+            btn11.Name = "btn11";
+            btn11.Size = new Size(230, 34);
+            btn11.TabIndex = 19;
+            btn11.Text = "Rome";
+            btn11.UseVisualStyleBackColor = true;
+            btn11.Click += btn11_Click;
+            // 
+            // btn12
+            // 
+            btn12.Location = new Point(314, 497);
+            btn12.Name = "btn12";
+            btn12.Size = new Size(230, 34);
+            btn12.TabIndex = 20;
+            btn12.Text = "London";
+            btn12.UseVisualStyleBackColor = true;
+            btn12.Click += btn12_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(81, 558);
+            label5.Name = "label5";
+            label5.Size = new Size(219, 15);
+            label5.TabIndex = 21;
+            label5.Text = "Who wrote the play \"Romeo and Juliet\"?";
+            // 
+            // btn13
+            // 
+            btn13.Location = new Point(78, 592);
+            btn13.Name = "btn13";
+            btn13.Size = new Size(230, 34);
+            btn13.TabIndex = 22;
+            btn13.Text = "Jane Austen";
+            btn13.UseVisualStyleBackColor = true;
+            btn13.Click += btn13_Click;
+            // 
+            // btn14
+            // 
+            btn14.Location = new Point(314, 592);
+            btn14.Name = "btn14";
+            btn14.Size = new Size(230, 34);
+            btn14.TabIndex = 23;
+            btn14.Text = "Charles Dickens";
+            btn14.UseVisualStyleBackColor = true;
+            btn14.Click += btn14_Click;
+            // 
+            // btn15
+            // 
+            btn15.Location = new Point(78, 632);
+            btn15.Name = "btn15";
+            btn15.Size = new Size(230, 34);
+            btn15.TabIndex = 24;
+            btn15.Text = "William Shakespeare";
+            btn15.UseVisualStyleBackColor = true;
+            btn15.Click += btn15_Click;
+            // 
+            // btn16
+            // 
+            btn16.Location = new Point(314, 632);
+            btn16.Name = "btn16";
+            btn16.Size = new Size(230, 34);
+            btn16.TabIndex = 25;
+            btn16.Text = "Mark Twain";
+            btn16.UseVisualStyleBackColor = true;
+            btn16.Click += btn16_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 829);
+            Controls.Add(btn16);
+            Controls.Add(btn15);
+            Controls.Add(btn14);
+            Controls.Add(btn13);
+            Controls.Add(label5);
+            Controls.Add(btn12);
+            Controls.Add(btn11);
+            Controls.Add(btn10);
+            Controls.Add(btn9);
+            Controls.Add(label4);
+            Controls.Add(btn8);
+            Controls.Add(btn7);
+            Controls.Add(btn6);
+            Controls.Add(btn5);
+            Controls.Add(label3);
+            Controls.Add(btn4);
+            Controls.Add(btn3);
+            Controls.Add(btn2);
+            Controls.Add(btn1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Button btn1;
+        private Button btn2;
+        private Button btn3;
+        private Button btn4;
+        private Label label3;
+        private Button btn5;
+        private Button btn6;
+        private Button btn7;
+        private Button btn8;
+        private Label label4;
+        private Button btn9;
+        private Button btn10;
+        private Button btn11;
+        private Button btn12;
+        private Label label5;
+        private Button btn13;
+        private Button btn14;
+        private Button btn15;
+        private Button btn16;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,100 @@
+namespace Quiz_Game_GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            this.btn1.BackColor = Color.Green;
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            this.btn2.BackColor = Color.Red;
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            this.btn3.BackColor = Color.Red;
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            this.btn4.BackColor = Color.Red;
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            this.btn5.BackColor = Color.Red;
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            this.btn6.BackColor = Color.Red;
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            this.btn7.BackColor = Color.Red;
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            this.btn8.BackColor = Color.Green;
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            this.btn9.BackColor = Color.Red;
+        }
+
+        private void btn10_Click(object sender, EventArgs e)
+        {
+            this.btn10.BackColor = Color.Green;
+        }
+
+        private void btn11_Click(object sender, EventArgs e)
+        {
+            this.btn11.BackColor = Color.Red;
+        }
+
+        private void btn12_Click(object sender, EventArgs e)
+        {
+            this.btn12.BackColor = Color.Red;
+        }
+
+        private void btn13_Click(object sender, EventArgs e)
+        {
+            this.btn13.BackColor = Color.Red;
+        }
+
+        private void btn14_Click(object sender, EventArgs e)
+        {
+            this.btn14.BackColor = Color.Red;
+        }
+
+        private void btn15_Click(object sender, EventArgs e)
+        {
+            this.btn15.BackColor = Color.Green;
+        }
+
+        private void btn16_Click(object sender, EventArgs e)
+        {
+            this.btn16.BackColor = Color.Red;
+        }
+    }
+}
